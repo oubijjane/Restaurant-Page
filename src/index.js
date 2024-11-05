03f2608b6d6939c5330ecd46b212396cc4f87a40
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-11-05 11:08:24 +0100

    set up the webpack and initial layout for the page

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+import "./rests.jpg";
+
+
+console.log("test>>>>>>>123")
---
+++ src/page-load.js
@@ -0,0 +1,14 @@
+function loadPage () {
+    const div = document.querySelector("content");
+    const header = document.createElement("h1");
+    const greeting = document.createElement("p");
+    const smallDescription = document.createElement("p");
+    const picture = document.createElement("img");
+    const Description = document.createElement("p");
+    const bestDishs = document.createElement("ul");
+    const bestDish1 = document.createElement("li");
+    const bestDish2 = document.createElement("li");
+    const bestDish3 = document.createElement("li");
+    const secondeGreeting = document.createElement("p");
+
+}
